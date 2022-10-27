01e90ef7f942daeec4b66597be74f693949a7439
Tajbid Hasib <tajbid.hasib@dynatrace.com>
2022-10-26 22:37:13 -0400

    2

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
-
-const comments = ["Did you get a chance to review my previous comment?", "Just checking in again, did you get a chance to review my previous comments?", "I hope all is well as I have not heard from you in a while. If we do not hear from you tomorrow then we will go ahead and close the ticket. However, please keep in mind that you can simply re-open the ticket or create a follow-up."];
+// Configure your comments:
+const comments = ["Did you get a chance to review my previous comment?", 
+"Just checking in again, did you get a chance to review my previous comments?", 
+"I hope all is well as I have not heard from you in a while. If we do not hear from you tomorrow then we will go ahead and close the ticket. However, please keep in mind that you can simply re-open the ticket or create a follow-up."];
 
 const template = document.getElementById("li_template");
 const elements = new Set();
